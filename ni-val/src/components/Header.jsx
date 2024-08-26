3ac81ea5dcf2f6eca5d777c0c45d62b7c7100a06
Sahil <parvezsayyad0045@gmail.com>
2024-08-26 20:59:44 +0530

    initialized website

---
+++ ni-val/src/components/Header.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <nav className="navbar navbar-expand-lg navbar-light">
+                <div className="container d-flex justify-content-between align-items-center">
+                    {/* Brand */}
+                    <a className="navbar-brand" href="#"><img src="/images/ni-val-brand.png" alt="Logo" /></a>
+
+                    {/* Offcanvas Toggler */}
+                    <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    {/* Offcanvas Menu */}
+                    <div className="offcanvas offcanvas-end d-lg-none" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                        <div className="offcanvas-header">
+                            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div className="offcanvas-body">
+                            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                                <li className="nav-item">
+                                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Services</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">About</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Contact</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    {/* Normal Navbar Links (visible on large screens) */}
+                    <div className="collapse navbar-collapse d-none d-lg-flex">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Services</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">About</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
