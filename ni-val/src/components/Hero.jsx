74599b153d0c0b8cfbb8ce9eb66c23fea2273c41
Sahil <parvezsayyad0045@gmail.com>
2024-08-26 23:29:15 +0530

    added hero page

---
+++ ni-val/src/components/Hero.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <section className="hero">
+            <div className="container">
+                <div className="row align-items-center align-items-stretch">
+                    <div className="col-md-6">
+                        <h1 className='hero-heading text-light'>Data-Driven Email Marketing for Lead Growth</h1>
+                        <p className="here-content text-light">Harness the power of automation and analytics to build stronger relationships and increase sales.</p>
+                        <button></button>
+                        <ul class="nav pt-5 fs-5">
+                            <li class="nav-item"><a href="" class="nav-link text-white"><i
+                                class="fa-brands fa-linkedin"></i></a></li>
+                            <li class="nav-item"><a href="" class="nav-link text-white"><i
+                                class="fa-brands fa-x-twitter"></i></a></li>
+                            <li class="nav-item"><a href="" class="nav-link text-white"><i
+                                class="fa-brands fa-facebook-f"></i></a></li>
+                            <li class="nav-item"><a href="" class="nav-link text-white"><i
+                                class="fa-brands fa-instagram"></i></a></li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
